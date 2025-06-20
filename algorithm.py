44551889cfe64edf930c50178e58357c000f31d3
İbrahim Taha Pınar <154216157+itahapinar@users.noreply.github.com>
2025-06-20 22:39:51 +0000

    Update code for Streamlit app

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,7 @@
-import numpy as np
-
 def simplex(c, A, b):
     m, n = A.shape
     tableau = np.hstack([A, np.eye(m), b.reshape(-1, 1)])
-    cost = np.hstack([c, np.zeros(m + 1)])
+    cost = np.hstack([-c, np.zeros(m + 1)])  
     tableau = np.vstack([tableau, cost])
 
     basis = list(range(n, n + m))
@@ -13,16 +11,12 @@ def simplex(c, A, b):
         if tableau[-1, pivot_col] >= 0:
             break
 
-        ratios = []
-        for i in range(m):
-            if tableau[i, pivot_col] > 0:
-                ratios.append(tableau[i, -1] / tableau[i, pivot_col])
-            else:
-                ratios.append(np.inf)
-
-        pivot_row = np.argmin(ratios)
-        if ratios[pivot_row] == np.inf:
+        valid_rows = [i for i in range(m) if tableau[i, pivot_col] > 0]
+        if not valid_rows:
             raise Exception("Unbounded solution")
+
+        ratios = [tableau[i, -1] / tableau[i, pivot_col] for i in valid_rows]
+        pivot_row = valid_rows[np.argmin(ratios)]
 
         pivot_element = tableau[pivot_row, pivot_col]
         tableau[pivot_row] /= pivot_element
@@ -37,4 +31,5 @@ def simplex(c, A, b):
     for i in range(m):
         solution[basis[i]] = tableau[i, -1]
 
-    return solution[:n], tableau
+    z_value = tableau[-1, -1] * -1
+    return solution[:n], z_value, tableau

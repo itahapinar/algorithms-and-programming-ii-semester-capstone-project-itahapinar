18b85a66b3ae53fcc1b4a41364a9fac19030fc02
İbrahim Taha Pınar <154216157+itahapinar@users.noreply.github.com>
2025-06-21 08:58:40 +0000

    Update code for Streamlit app

--- test-algorithm.py
+++ test-algorithm.py
@@ -2,16 +2,12 @@ import unittest
 import numpy as np
 from algorithm import simplex
 
-class TestSimplexMethod(unittest.TestCase):
-
-    def test_basic_case(self):
+class TestSimplex(unittest.TestCase):
+    def test_maximize(self):
         A = np.array([[1, 1], [2, 1]])
         b = np.array([4, 5])
-        c = np.array([-3, -2])
-
+        c = np.array([3, 2])
         x, max_val, _ = simplex(A, b, c)
-
-        # Check if the result is close to the known optimal solution
         self.assertAlmostEqual(x[0], 1, places=2)
         self.assertAlmostEqual(x[1], 3, places=2)
         self.assertAlmostEqual(max_val, 9.0, places=2)

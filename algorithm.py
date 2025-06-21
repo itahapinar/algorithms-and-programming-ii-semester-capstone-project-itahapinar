b89dc707b9caba40039de284055b6cf5d6c1fac4
İbrahim Taha Pınar <154216157+itahapinar@users.noreply.github.com>
2025-06-21 09:58:39 +0000

    Update code for Streamlit app

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 def simplex(c, A, b):
     m, n = A.shape
     tableau = np.hstack([A, np.eye(m), b.reshape(-1, 1)])
@@ -9,8 +7,12 @@ def simplex(c, A, b):
     steps = []  # Pivot adımlarını tut
     while any(tableau[-1, :-1] < 0):
         pivot_col = np.argmin(tableau[-1, :-1])
-        ratios = tableau[:-1, -1] / tableau[:-1, pivot_col]
-        ratios[ratios < 0] = np.inf
+
+        pivot_col_values = tableau[:-1, pivot_col]
+        ratios = np.full_like(pivot_col_values, np.inf, dtype=float)
+        valid = pivot_col_values > 1e-10
+        ratios[valid] = tableau[:-1, -1][valid] / pivot_col_values[valid]
+
         pivot_row = np.argmin(ratios)
         pivot = tableau[pivot_row, pivot_col]
         tableau[pivot_row] /= pivot
@@ -28,4 +30,5 @@ def simplex(c, A, b):
             row = list(col).index(1)
             solution[i] = tableau[row, -1]
 
-    return solution, tableau[-1, -1], steps
+    return solution, -tableau[-1, -1], steps
+

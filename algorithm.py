8e9540384ca08c2826f294a5adcf67f4c2d911a5
İbrahim Taha Pınar <154216157+itahapinar@users.noreply.github.com>
2025-06-21 08:56:00 +0000

    Update code for Streamlit app

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,17 @@
 import numpy as np
+from scipy.optimize import linprog
 
 def simplex(A, b, c):
-    from scipy.optimize import linprog
-    res = linprog(c=-c, A_ub=A, b_ub=b, method="simplex")
-    tableau = np.array([
-        [1, 1, 2, -1, 3],
-        [0, 1, -1, 1, 1],
-        [0, 0, 1, 1, 9]
-    ])
-    return res.x, -res.fun, tableau
+    # Negate the coefficients to convert maximization to minimization
+    res = linprog(c=-c, A_ub=A, b_ub=b, method="highs")  # "simplex" is deprecated
+    if res.success:
+        max_value = np.dot(c, res.x)  # Use original c here to get max value
+        # Return dummy tableau for display
+        tableau = np.array([
+            [1, 1, 2, -1, 3],
+            [0, 1, -1, 1, 1],
+            [0, 0, 1, 1, 9]
+        ])
+        return res.x, max_value, tableau
+    else:
+        return None, None, None
